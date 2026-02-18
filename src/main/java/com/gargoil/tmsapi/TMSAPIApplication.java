9a126d4bd6581199fa7ae28c6aa1839af5a69d79
Gargoil <emmanueledusei.k@gmail.com>
2026-02-18 09:39:43 +0000

    Initialize Spring Boot project with Maven wrapper

---
+++ src/main/java/com/gargoil/tmsapi/TMSAPIApplication.java
@@ -0,0 +1,11 @@
+package com.gargoil.tmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TMSAPIApplication {
+    static void main(String[] args) {
+        SpringApplication.run(TMSAPIApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gargoil/tmsapi/TMSAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gargoil.tmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TMSAPIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
